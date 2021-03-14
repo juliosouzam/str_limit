3d17a88e7c8cd44640f7853a65560a3c3234d6a2
Júlio César <julio.souzam@gmail.com>
2021-03-14 15:34:52 -0300

    test: add some more tests condition

--- __tests__/str-limit.spec.ts
+++ __tests__/str-limit.spec.ts
@@ -1,7 +1,34 @@
-import { str_limit } from '../src';
+import { str_limit } from "../src";
 
-test('if a string was limited by 20', () => {
-  const word = str_limit(`HP 24mh FHD Monitor - Computer Monitor with 23.8-Inch IPS Display`, 20);
+test("if a string was limited by default (30)", () => {
+  const word = str_limit(
+    `HP 24mh FHD Monitor - Computer Monitor with 23.8-Inch IPS Display`
+  );
 
-  expect(word).toBe('HP 24mh FHD Monitor...');
+  expect(word).toBe("HP 24mh FHD Monitor - Computer...");
+});
+
+test("if a string was limit passing a length 20", () => {
+  const word = str_limit(
+    `HP 24mh FHD Monitor - Computer Monitor with 23.8-Inch IPS Display`,
+    20
+  );
+
+  expect(word).toBe("HP 24mh FHD Monitor...");
+});
+
+test("if a string was limit passing a length 20 and passing a ending symbol", () => {
+  const word = str_limit(
+    `HP 24mh FHD Monitor - Computer Monitor with 23.8-Inch IPS Display`,
+    20,
+    "###"
+  );
+
+  expect(word).toBe("HP 24mh FHD Monitor###");
+});
+
+test("if a string length is less then a lenght limit", () => {
+  const word = str_limit(`HP 24mh`);
+
+  expect(word).toBe("HP 24mh");
 });
